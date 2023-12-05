5d2c83726b5632bfdf900c54a74eeaed8d861a62
SAMKE-glitch <sammythemwa@gmail.com>
2023-12-06 02:16:26 +0300

    adde floors

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -1,7 +1,7 @@
 import Building from './5-building';
 
 export default class SkyHighBuilding extends Building {
-  constructor(sqft, floor) {
+  constructor(sqft, floors) {
     super(sqft);
     this._floors = floors;
   }
